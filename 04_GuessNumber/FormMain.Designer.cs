ba3e3f73a31d67e52fca7b187bfc1c32da0c7ddf
SebastianWojczyk <62283873+SebastianWojczyk@users.noreply.github.com>
2024-10-15 15:17:25 +0200

    Add files via upload

---
+++ 04_GuessNumber/FormMain.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace _04_GuessNumber
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLess = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonMore = new System.Windows.Forms.Button();
+            this.labelNumber = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonLess
+            // 
+            this.buttonLess.Location = new System.Drawing.Point(12, 78);
+            this.buttonLess.Name = "buttonLess";
+            this.buttonLess.Size = new System.Drawing.Size(75, 23);
+            this.buttonLess.TabIndex = 0;
+            this.buttonLess.Text = "Less";
+            this.buttonLess.UseVisualStyleBackColor = true;
+            this.buttonLess.Click += new System.EventHandler(this.ButtonLess_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(93, 78);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.ButtonOK_Click);
+            // 
+            // buttonMore
+            // 
+            this.buttonMore.Location = new System.Drawing.Point(174, 78);
+            this.buttonMore.Name = "buttonMore";
+            this.buttonMore.Size = new System.Drawing.Size(75, 23);
+            this.buttonMore.TabIndex = 2;
+            this.buttonMore.Text = "More";
+            this.buttonMore.UseVisualStyleBackColor = true;
+            this.buttonMore.Click += new System.EventHandler(this.ButtonMore_Click);
+            // 
+            // labelNumber
+            // 
+            this.labelNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelNumber.Location = new System.Drawing.Point(12, 21);
+            this.labelNumber.Name = "labelNumber";
+            this.labelNumber.Size = new System.Drawing.Size(237, 54);
+            this.labelNumber.TabIndex = 3;
+            this.labelNumber.Text = "label1";
+            this.labelNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(9, 9);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(0, 13);
+            this.labelInfo.TabIndex = 4;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 111);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.labelNumber);
+            this.Controls.Add(this.buttonMore);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonLess);
+            this.MaximumSize = new System.Drawing.Size(277, 150);
+            this.MinimumSize = new System.Drawing.Size(277, 150);
+            this.Name = "FormMain";
+            this.Text = "Guess the number v0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLess;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonMore;
+        private System.Windows.Forms.Label labelNumber;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
+
---
+++ 04_GuessNumber/FormMain.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_GuessNumber
+{
+    public partial class FormMain : Form
+    {
+        private int MinValue;
+        private int MaxValue;
+        private int CurrentValue;
+        public FormMain()
+        {
+            InitializeComponent();
+            PrepareNewGame();
+        }
+
+        private void ButtonOK_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+
+            result = MessageBox.Show("Again?",
+                                     $"Your number is {CurrentValue}.",
+                                     MessageBoxButtons.YesNo);
+
+            if(result == DialogResult.Yes)
+            {
+                PrepareNewGame();
+            }
+            else if(result == DialogResult.No)
+            {
+                this.Close();
+            }
+        }
+
+        private void ButtonLess_Click(object sender, EventArgs e)
+        {
+            MaxValue = CurrentValue - 1;
+            PrepareNextStep();
+        }
+
+        private void ButtonMore_Click(object sender, EventArgs e)
+        {
+            MinValue = CurrentValue + 1;
+            PrepareNextStep();
+        }
+
+        private void PrepareNewGame()
+        {
+            MinValue = 1;
+            MaxValue = 100;
+
+            buttonLess.Enabled = true;
+            buttonMore.Enabled = true;
+
+            PrepareNextStep();
+        }
+
+        private void PrepareNextStep()
+        {
+            CurrentValue = (MinValue + MaxValue)/ 2;
+
+            if(CurrentValue<= MinValue)
+            {
+                buttonLess.Enabled = false;
+            }
+            if (CurrentValue >= MaxValue)
+            {
+                buttonMore.Enabled = false;
+            }
+
+            labelNumber.Text = CurrentValue.ToString();
+            labelInfo.Text = $"Value from range [{MinValue}, {MaxValue}]";
+        }
+    }
+}
